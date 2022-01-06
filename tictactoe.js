fc3f31daf6136cabff8393b1ba6a986bb23757ac
Charlotte France <charlie.france@ntlworld.com>
2022-01-06 22:17:12 +0000

    tried to start java

---
+++ tictactoe.js
@@ -0,0 +1,31 @@
+const statusUpdate = document.querySelector('status')
+
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", ""];
+
+const winner = () => `Player ${currentPlayer}! You've won!`;
+const draw = () => `Its a draw!`;
+const currentPlayerTurn = () => `It's your turn ${currentPlayer}!`
+
+statusUpdate.innerHTML = currentPlayerTurn();
+
+function handleSquarePlayed(clickedSquare, clickedSquareIndex) {
+    gameState[clickedSquareIndex] = currentPlayer;
+    clickedSquare.innerHTML = currentPlayer;
+}
+
+function handleSquareClick(clickedSquareEvent) {
+    const clickedSquare = clickedSquareEvent.target;
+    const clickedSquareIndex = parseInt(
+        clickedSquare.getAttribute('grid-number')
+    );
+    if (gameState[clickedSquareIndex] !== "" || !gameActive) {
+        return;
+    }
+    handleSquarePlayed(clickedSquare, clickedSquareIndex);
+    handleResultValidation();
+}
+
+document.querySelectorAll('.grid').forEach(grid => grid.addEventListener('click', handleSquarePlayed));
+document.querySelector('.restart').addEventListener('click', handleRestartGame);
