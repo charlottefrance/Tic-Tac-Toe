c222c1270bb397f1cd7edf89b22a43555895c5c1
Charlotte France <charlie.france@ntlworld.com>
2022-01-08 18:36:53 +0000

    did game rules

--- tictactoe.js
+++ tictactoe.js
@@ -32,6 +32,49 @@ function handleSquareClick(clickedSquareEvent) {
     handleResultValidation();
 }
 
+const winningPossibility = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gamestate[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break
+        }
+    }
+
+    if (roundWon) {
+        statusDisplay.innerHTML = winningMessage();
+        gameActive = false;
+        return
+    }
+
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return;
+    }
+
+    handlePlayerChange();
+}
+
 
 document.querySelectorAll('.grid').forEach(grid => grid.addEventListener('click', handleSquarePlayed));
 document.querySelector('.restart').addEventListener('click', handleRestartGame);
